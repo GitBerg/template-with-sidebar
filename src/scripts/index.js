10fda93b37a6972a65ae50668cddbcae4913f0aa
Gutemberg Silva Filho <beguinhoboy@hotmail.com>
2021-07-25 01:28:53 -0300

    Finished sidebar

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector('#menu');
+let sidebar = document.querySelector('.sidebar');
+let search = document.querySelector('.bx-search');
+
+function showSideBar(){
+            sidebar.classList.toggle("active");
+}
